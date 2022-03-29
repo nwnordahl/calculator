366c70ab6246358f6b61ab21a732191bf760d964
nwnordahl <nicolaiwnordahl@gmail.com>
2022-03-29 13:35:00 +0200

    Set up project files and add basic arithmetic functions

---
+++ script.js
@@ -0,0 +1,19 @@
+function add(number1, number2) {
+  return number1 + number2;
+}
+
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
+function divide(number1, number2) {
+  return number1 / number2;
+}
+
+function operate(functionName, number1, number2) {
+  return functionName(number1, number2);
+}
